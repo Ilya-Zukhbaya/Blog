8474c9b9646bf3b0443b4d8583b78714c1a5d48e
ILya-Zukhbaya <ne_perepletov@mail.ru>
2022-09-03 01:48:39 +0300

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect('mongodb+srv://admin:www111@cluster0.74hmhhq.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.error(err));
 
 const app = express();
 
@@ -10,9 +17,17 @@ app.get('/', (req, res) => {
 
 app.post('/auth/login', (req, res) => {
   console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: req.body.fullName,
+    },
+    'secret',
+  );
   res.json({
     success: true,
-    confirm: req.body,
+    token,
   });
 });
 
